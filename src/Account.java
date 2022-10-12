864676779e718451014b88203ef3b6ac3e6c7672
pmuldowney <pmuldowney@vassar.edu>
2022-10-12 18:00:31 -0400

    created Account class

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,16 @@
 public class Account {
+    
+    //fields
+    public String name = "";
+    public String username = "";
+    public String email = "";
+    private final String password = "";
+    
+    //constructor
+    private Account (String username, String password, String name, String email){
+        username = this.username;
+        password = this.password;
+        name = this.name;
+        email = this.email;
+    }
 }
