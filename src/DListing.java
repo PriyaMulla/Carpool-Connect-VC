b7e146c2639d19ebee090e2459637938a3460ba3
Christopher CortazoRosas <ccortazorosas@mail.cs.vassar.edu>
2022-10-13 18:54:18 -0400

    Created DListing

---
+++ src/DListing.java
@@ -0,0 +1,18 @@
+public class DListing {
+
+    //fields
+    int dateCreated = 0;
+    int dateOfTrip = 0;
+    int timeOfTrip = 0;
+    String startLocation = "";
+    String endLocation = "";
+    
+    //constructor
+    private DListing(int date, int time, String start, String end){
+        this.dateOfTrip = this.dateOfTrip;
+        this.timeOfTrip = this.timeOfTrip;
+        this.startLocation = this.startLocation;
+        this.endLocation = this.endLocation;
+    }
+    
+}
