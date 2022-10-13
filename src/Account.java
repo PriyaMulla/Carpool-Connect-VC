f6808b298d4fd67c3019b1367a273e2aa7ca069f
Christopher CortazoRosas <ccortazorosas@mail.cs.vassar.edu>
2022-10-13 18:21:09 -0400

    Edited code

--- src/Account.java
+++ src/Account.java
@@ -4,13 +4,13 @@ public class Account {
     public String name = "";
     public String username = "";
     public String email = "";
-    private final String password = "";
+    private String password = "";
     
     //constructor
     private Account (String username, String password, String name, String email){
-        username = this.username;
-        password = this.password;
-        name = this.name;
-        email = this.email;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
     }
 }
