c193c52383a3f706fce108e0e80ff932db27210f
pmuldowney <pmuldowney@vassar.edu>
2022-10-13 19:20:14 -0400

    created DListing

--- src/DListing.java
+++ src/DListing.java
@@ -1,18 +1,33 @@
-public class DListing {
+public class DListing extends AListing{
 
     //fields
-    int dateCreated = 0;
-    int dateOfTrip = 0;
-    int timeOfTrip = 0;
-    String startLocation = "";
-    String endLocation = "";
-    
-    //constructor
-    private DListing(int date, int time, String start, String end){
-        this.dateOfTrip = this.dateOfTrip;
-        this.timeOfTrip = this.timeOfTrip;
-        this.startLocation = this.startLocation;
-        this.endLocation = this.endLocation;
+    int dateCreated =0;
+    int dateOfTrip =0;
+    int timeOfTrip =0;
+    String startLocation ="";
+    String endLocation ="";
+    int listingID =0;
+    int seats =0;
+
+    private DListing(int date, int time, String start, String end, int listingID, int seats){
+        this.dateOfTrip = date;
+        this.timeOfTrip = time;
+        this.startLocation = start;
+        this.endLocation = end;
+        this.listingID = listingID;
+        this.seats = seats;
     }
-    
+
+    @Override
+    public String toString() {
+        return "DListing{" +
+                "dateCreated=" + dateCreated +
+                ", dateOfTrip=" + dateOfTrip +
+                ", timeOfTrip=" + timeOfTrip +
+                ", startLocation='" + startLocation + '\'' +
+                ", endLocation='" + endLocation + '\'' +
+                ", listingID=" + listingID +
+                ", seats=" + seats +
+                '}';
+    }
 }
