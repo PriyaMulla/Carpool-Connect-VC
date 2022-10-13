5954686f6e833ca1425329976ec466051e7f91ca
pmuldowney <pmuldowney@vassar.edu>
2022-10-13 18:51:18 -0400

    updated controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,7 @@
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.List;
+
+
 
 public class Controller {
 
@@ -16,28 +18,15 @@ public class Controller {
         return;
     }
 
-    public LinkedList<Listing> searchListings(){
-        return new LinkedList<Listing>();
+    public List<AListing> searchListings(){
+        return new LinkedList<AListing>();
     }
 
-    public LinkedList<Listing> filterListings(){
-        return new LinkedList<Listing>();
+    public List<AListing> filterListings(){
+        return new LinkedList<AListing>();
     }
 
     public static void main(String[] args){
-        System.out.println("Hello and welcome to account creation");
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a username:");
-        String user = scan.nextLine();
-
-        System.out.println("Enter a password:");
-        String password = scan.nextLine();
-
-        System.out.println("Enter your name");
-        String name = scan.nextLine();
-
-        System.out.println("Enter an email address");
-        String email = scan.nextLine();
     }
 }
