8df559e4c221774d2774b8aed86c005361234879
Christopher CortazoRosas <ccortazorosas@efcodd.cs.vassar.edu>
2022-10-14 15:15:33 -0400

    Edited controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,23 +1,20 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Date;
+import java.util.*;
 import java.text.SimpleDateFormat;
 import java.time.*;
 
 public class Controller{
+    List<AListing> PageOfListing = new ArrayList<>();
     //Account creation
     public boolean isValidUsername(String input) {
         return input.length() > 5;
     }
     public boolean isValidPassword(String input){
-        return (input.length() > 5) & (Character.isUpperCase(input.charAt(0))) & ((input.contains("/")) | (input.contains("*")) | (input.contains("@")) | (input.contains("#")) | (input.contains("$")) | (input.contains("&")));
+        return (input.length() > 5) && (Character.isUpperCase(input.charAt(0))) & ((input.contains("!"))|(input.contains("_"))|(input.contains("/")) | (input.contains("*")) | (input.contains("@")) | (input.contains("#")) | (input.contains("$")) | (input.contains("&")));
     }
     public boolean isValidEmail(String input){
-        return ((input.contains("@vassar.edu")) & (input.length() > 11));
+        return ((input.contains("@vassar.edu")) && (input.length() > 11));
     }
 
-    //Listing creation?
     public Account createAccount(String username, String password,String name,String email){
         Account acc = new Account(username,password,name,email);
         return acc;
@@ -26,20 +23,14 @@ public class Controller{
     public AListing createListing(int date, int time, String start, String end, int listingID,int seats, int who){
         //An attribute would be the current time and maybe date
         //turn the string of a date into actual time and then compare
-
         AListing listy;
         if(who == 0) {
             listy = new PListing(date, time, start, end,listingID,seats);
         } else{
             listy = new DListing(date,time,start,end,listingID,seats);
         }
+        PageOfListing.add(listy);
         return listy;
-    }
-
-    HashMap<Integer, AListing> hash_map = new HashMap<Integer, AListing>();
-//Method to add a Listing to PageOfListings
-    public void addToList(Object listing){
-        //hash_map.put(listing.get(listingID));
     }
 
     public List<AListing> searchListings(){
