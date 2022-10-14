3337316552d4f6daf4e6a090b0bc95406f151b91
Christopher CortazoRosas <ccortazorosas@mail.cs.vassar.edu>
2022-10-13 20:58:11 -0400

    Edited controller class

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,44 @@
 import java.util.LinkedList;
 import java.util.List;
 
+public class Controller{
+    String string = "Java";
+    String substring = "va";
 
-
-public class Controller {
-
+//System.out.println(string.contains(substring));
     //methods
-    public boolean isValidString(){
+    public boolean hasSpecialChar(){
+//*/@#$&
+    }
+
+    public boolean isValidUsername(String input) {
+        if(input.length() > 5){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean isValidPassword(String input){
+        if(input.length() > 5)
+    }
+    public boolean isValidEmail(String input){
+
+    }
+     public boolean isValidString(String input){
+        if(input.equals(input.compareTo("user"))){
+            if((input.length() > 5) & (Character.isUpperCase(input.charAt(0))) & ((input.contains("/")) |(input.contains("*")) | (input.contains("@")) | (input.contains("#")) | (input.contains("$")) | (input.contains("&")))){
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        } else if(input.equals(input.compareTo("email"))){
+
+        }
+
         return false;
     }
 
