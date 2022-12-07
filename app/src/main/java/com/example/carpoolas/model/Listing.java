3b494ac880adef1d0a2fa9f4181a12cf7b6f57ec
pmuldowney <pmuldowney@vassar.edu>
2022-12-06 20:18:41 -0500

    implementing persistant state

--- app/src/main/java/com/example/carpoolas/model/Listing.java
+++ app/src/main/java/com/example/carpoolas/model/Listing.java
@@ -1,22 +1,22 @@
 package com.example.carpoolas.model;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
+import java.io.Serializable;
 import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.regex.Pattern;
 
-public class Listing {
+public class Listing implements Serializable {
     //fields
     Date dateCreated;
     String role;
     Date dateTimeOfTrip;
-
     String startLocation;
-
     String endLocation;
     int seats;
 
@@ -27,9 +27,7 @@ public class Listing {
     public String getRole() {
         return role;
     }
-    public String getEndLocation() {
-        return endLocation;
-    }
+    public String getEndLocation() {return endLocation;}
 
     public Date getDateTimeOfTrip() {
         return dateTimeOfTrip;
@@ -43,16 +41,15 @@ public class Listing {
         return seats;
     }
 
+    public Listing() throws ParseException {};
 
-
-    public Listing(Date created, String role, Date dateTime, String start, String end,  int seats){
+    public Listing(Date created, String role, Date dateTime, String start, String end,  int seats) throws ParseException {
         this.dateCreated = created;
         this.role = role;
         this.dateTimeOfTrip = dateTime;
         this.startLocation = start;
         this.endLocation = end;
         this.seats = seats;
-
     }
 
 
@@ -71,7 +68,7 @@ public class Listing {
 
     //object for date formatting
     @SuppressLint("SimpleDateFormat")
-    SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+    static SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 
     @NonNull
     @Override
@@ -84,4 +81,50 @@ public class Listing {
                 "\n End: " + endLocation +
                 "\n Seats: " + seats;
     }
+
+    //convert Date to string
+    String strDateCreated = formatter.format(dateCreated);
+    String strDateTrip = formatter.format(dateTimeOfTrip);
+
+    //constants for (de) serialization
+    private static final String DATE_CREATED = "dateCreated";
+    private static final String ROLE = "role";
+    private static final String DATE_TIME = "dateTimeOfTrip";
+    private static final String START_LOCATION = "startLocation";
+    private static final String END_LOCATION = "endLocation";
+    private static final String SEATS = "seats";
+
+    /**
+     * Converts the Listing the method is called on into a Bundle.
+     *
+     * @return A Bundle with the listing's contents
+     */
+    Bundle b = new Bundle();
+    @NonNull
+    public Bundle toBundle(){
+        b.putString(DATE_CREATED, this.strDateCreated);
+        b.putString(ROLE, this.role);
+        b.putString(DATE_TIME, this.strDateTrip);
+        b.putString(START_LOCATION, this.startLocation);
+        b.putString(END_LOCATION, this.endLocation);
+        b.putInt(SEATS, this.seats);
+        return b;
+    }
+
+
+
+    /**
+     * Creates and returns a Listing from a previously created Bundle.
+     *
+     * @param b the Bundle to convert to a Listing
+     * @return the Listing representation of the input Bundle
+     */
+    @NonNull
+    public static Listing fromBundle(@NonNull Bundle b) throws ParseException {
+        //switch dates back to date type
+        Date bDateCreated = formatter.parse(b.getString(DATE_CREATED));
+        Date bDateTrip = formatter.parse(b.getString(DATE_TIME));
+        return new Listing(bDateCreated, b.getString(ROLE), bDateTrip, b.getString(START_LOCATION), b.getString(END_LOCATION), b.getInt(SEATS));
+    }
+
 }
---
+++ app/src/main/java/com/example/carpoolas/persistance/FirestoreFacade.java
@@ -0,0 +1,122 @@
+package com.example.carpoolas.persistance;
+import androidx.annotation.NonNull;
+
+import com.example.carpoolas.model.CollectionOfListings;
+import com.example.carpoolas.model.Listing;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+public class FirestoreFacade implements IPersistenceFacade{
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance(); // database connection
+
+    private static final String LISTINGS_COLLECTION = "LISTINGS_COLLECTION"; // listings collection name
+    //private static final String USERS_COLLECTION = "USERS_COLLECTION"; // users collection name
+
+
+    /* ledger-related methods start */
+
+    /**
+     * Saves the sale passed in as input to the underlying persistence solution.
+     * @param listing the listing to be saved
+     */
+    public void saveListing(Listing listing){
+        this.db.collection(LISTINGS_COLLECTION)
+                .add(listing); // creates new document with pseudorandom id, uses firestore's built-in serialization
+    }
+
+    /**
+     * Issues a CollectionOfListings retrieval operation.
+     * @param listener the listener to be notified when the collectionOfListings becomes available.
+     */
+    public void retrieveCollectionOfListings(DataListener<CollectionOfListings> listener){
+        this.db.collection(LISTINGS_COLLECTION)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() { // called when the data comes back from the database
+                    @Override
+                    public void onSuccess(QuerySnapshot qsnap) {
+                        CollectionOfListings collectionOfListings = new CollectionOfListings();
+                        for (DocumentSnapshot dsnap : qsnap){
+                            Listing listing = dsnap.toObject(Listing.class);
+                            collectionOfListings.addCreatedListing(listing);
+                        }
+                        listener.onDataReceived(collectionOfListings); // let the listener know we have a COL now
+                    }
+                });
+    }
+
+    /* ledger-related methods end */
+
+   /* *//* authentication-related methods start *//*
+
+    *//**
+     *  Creates an entry for the specified User in the underlying persistence subsystem.
+     *
+     * @param user the user to be created
+     * @param listener the observer to be notified of the query result. OnYesResult() is called if
+     *                 a new user was created. Conversely, OnNoResult() is called if a user with
+     *                 the specified username already existed.
+     *//*
+    @Override
+    public void createUserIfNotExists(@NonNull User user, @NonNull BinaryResultListener listener) {
+
+        String username = user.getUsername();
+
+        this.retrieveUser(username, new DataListener<User>() {
+            @Override
+            public void onDataReceived(@NonNull User data) {
+                listener.onNoResult();
+            }
+
+            @Override
+            public void onNoDataFound() {
+                db.collection(USERS_COLLECTION).document(username).
+                        set(user).
+                        addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                listener.onYesResult();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                listener.onNoResult();
+                            }
+                        });
+
+            }
+        });
+    }
+
+    *//**
+     * Retrieves the User with the specified username from the underlying persistence subsystem.
+     *
+     * @param username the username of the user to be retrieved.
+     * @param listener observer to be notified of query result. onDataReceived() is called if a
+     *                 user with the specified username was found. Otherwise, onNoDataFound() is
+     *                 called.
+     *//*
+    @Override
+    public void retrieveUser(@NonNull String username, @NonNull DataListener<User> listener) {
+
+        db.collection(USERS_COLLECTION).document(username).get().
+                addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                         @Override
+                                         public void onSuccess(DocumentSnapshot dsnap) {
+                                             if (dsnap.exists()){
+                                                 User user = dsnap.toObject(User.class);
+                                                 listener.onDataReceived(user);
+                                             } else // no user found
+                                                 listener.onNoDataFound();
+                                         }
+                                     }
+                );
+
+    }
+
+    *//* authentication-related methods end */
+}
